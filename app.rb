f34b4eeedd1f75cd237808d072bcddd1332cd5f0
Asia Kane and Ken Rutan <asia@>
2017-04-19 15:23:20 -0700

    add user interface files

---
+++ app.rb
@@ -0,0 +1,16 @@
+require('sinatra')
+require('sinatra/reloader')
+require('rspec')
+require('pry')
+require('./lib/palindromes')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
+
+get('/display') do
+  @word = params.fetch('word')
+  @output = @word.pal()
+  erb(:index)
+end
